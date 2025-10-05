452dcbb583d54e9ca9769674e75761e19b7b5e5f
nandneetu <nandkishornayak226@gmail.com>
2025-10-05 07:40:31 +0530

    Update vars.py

--- Modules/vars.py
+++ Modules/vars.py
@@ -3,12 +3,12 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22182189"))
-API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "27144144"))
+API_HASH = environ.get("API_HASH", "8154767f1ceaa27d65220e1842e0c313")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8479902379:AAFm-ScArFKFZLtiTpASHayesc2dNcqGcCg")
 
-OWNER = int(environ.get("OWNER", "5680454765"))
-CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
+OWNER = int(environ.get("OWNER", "1405287675"))
+CREDIT = environ.get("CREDIT", "Balaji bot")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
 TOTAL_USER = os.environ.get('TOTAL_USERS', '5680454765').split(',')
@@ -18,3 +18,4 @@ AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
+
